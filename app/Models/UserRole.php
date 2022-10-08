0cfc1426d2317e67c6390e576144d0650f95f17d
fajaragustian <12173512@bsi.ac.id>
2022-10-08 21:11:16 +0700

    stater

---
+++ app/Models/UserRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRole extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'role_id',
+    ];
+    // Relasi menghubungkan table role dan user dimana satu user dan satu role yaitu identitas
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2022_10_08_022704_create_user_role_id_table_on_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\Table;
+
+class CreateUserRoleIdTableOnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('user_role_id');
+            $table->foreign('user_role_id')->references('id')->on('user_roles')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign('user_role_id');
+        });
+    }
+}
